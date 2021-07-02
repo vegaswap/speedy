1f14edca6838f7a43019b3e68194b58c681a71ab
benjyz <ben@vegaswap.io>
2021-07-02 12:07:01 +0700

    set

--- default_settings.py
+++ default_settings.py
@@ -2,11 +2,11 @@
 DEBUG = True
 CORS_HEADERS = 'Content-Type'
 APP_VERSION = "dcecf29" #git rev-parse --short HEAD
-MONGO_CONN_STR = "mongodb+srv://ben:GmWGdBL3gpEJU9g@mcaps-olf5s.mongodb.net/test?retryWrites=true&w=majority"
-MONGO_DB = "mcaps_app"
+MONGO_CONN_STR = ""
+MONGO_DB = "app"
 REDIS_PORT = 6379
 REDIS_HOST = 'localhost'
 STATIC_URL_PATH = ''
-WEB3_INFURA_PROJECT_ID = 'bda859774de3410a9a900b75c1a2fa98'
+WEB3_INFURA_PROJECT_ID = ''
 APP_PORT = 8080
 APP_HOST = '0.0.0.0'
